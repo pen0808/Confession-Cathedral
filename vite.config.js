3cfa9cc6cf73ab02259e279cd1e97d5a5e3927c3
Paul Nwaoduah <nwaoduahpaul@gmail.com>
2026-05-23 08:56:50 +0100

    init

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Confession-Cathedral/',
   plugins: [react()],
-})
+  base: process.env.VERCEL ? "/" : "/confession-cathedrals/",
+}) 
